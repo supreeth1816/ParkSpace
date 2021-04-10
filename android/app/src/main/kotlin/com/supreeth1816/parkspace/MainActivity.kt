3e2e51a58adf5969787f2e7ef8932c9ca9b848b1
supreeth1816 <supreet.venkata@gmail.com>
2021-04-10 17:43:52 +0530

    Starter

---
+++ android/app/src/main/kotlin/com/supreeth1816/parkspace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.supreeth1816.parkspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ParkspaceApp());
+}
+
+class ParkspaceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+
+      home: ,
+    );
+  }
+}
