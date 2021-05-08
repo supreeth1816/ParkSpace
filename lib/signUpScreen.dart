e008d410415c2410b2aa0d89c7a4ceafe7598948
supreeth1816 <supreet.venkata@gmail.com>
2021-05-08 23:54:54 +0530

    Updated Login page

--- lib/signUpScreen.dart
+++ lib/signUpScreen.dart
@@ -3,7 +3,7 @@
 
 import 'package:flutter/material.dart';
 
-class ParkingVendorScreen extends StatelessWidget {
+class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
