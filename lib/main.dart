d26c294c71729958b4a2e318d24f33b7d90a919e
supreeth1816 <supreet.venkata@gmail.com>
2021-05-08 19:19:01 +0530

    Updated Login page

--- lib/main.dart
+++ lib/main.dart
@@ -26,63 +26,135 @@ class ParkspaceApp extends StatelessWidget {
   }
 }
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       backgroundColor: Colors.white,
 
-      body: Container(
-        padding: EdgeInsets.fromLTRB(40, 100, 40, 0),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 100, 40, 0),
 
 
-        child: Column(
+            child: Column(
 
 
-          children: [
+              children: [
 
-            Image.asset('assets/Logo.png',
-              width: 280,
+                Image.asset('assets/Logo.png',
+                  width: 280,
 
-            ),
+                ),
 
-            Text("ParkSpace",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w500,
-                color: Colors.black87,
-              ),
-            ),
-            SizedBox(height: 5,),
-            Text("Smart Parking Application",
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black54
-              ),
-            ),
-            SizedBox(height: 150,),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/user');
+                Text("ParkSpace",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 5,),
+                Text("Smart Parking Application",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black54
+                  ),
+                ),
+                SizedBox(height: 60,),
+
+            TextFormField(
+              controller: emailController,
+              // ignore: missing_return
+              validator: (String value){
+                if(value.isEmpty){
+                  print("Empty value by user");
+                }
               },
-              child: PrimaryButton(
-                btnText: "Login",
+
+              decoration: InputDecoration(
+                //   labelText: "First Name",
+                  hintText: "Enter email",
+                  hintStyle: TextStyle(
+                    fontSize: 15,
+                  ),
+                  contentPadding: const EdgeInsets.fromLTRB(14, 0, 10, 0),
+                  focusedBorder:OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple.shade500, width: 2.0),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
               ),
             ),
-            SizedBox(
-              height: 20,
+                SizedBox(height: 10,),
+
+                TextFormField(
+                  controller: passwordController,
+                  // ignore: missing_return
+                  validator: (String value){
+                    if(value.isEmpty){
+                      print("Empty value by user");
+                    }
+                  },
+
+                  decoration: InputDecoration(
+                    //   labelText: "First Name",
+                      hintText: "Enter password",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(14, 0, 10, 0),
+                      focusedBorder:OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple.shade500, width: 2.0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )
+                  ),
+                ),
+                SizedBox(height: 20,),
+
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/user');
+                  },
+                  child: PrimaryButton(
+                    btnText: "Login",
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/vendor');
+                    },
+                  child: OutlineBtn(
+                    btnText: "Sign Up",
+                  ),
+                )
+              ],
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/vendor');
-                },
-              child: OutlineBtn(
-                btnText: "Sign Up",
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
