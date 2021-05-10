a5be3691eeb66455206c8f88ba26aae1af61977d
supreeth1816 <supreet.venkata@gmail.com>
2021-05-11 04:51:33 +0530

    Updated Screen

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -102,31 +102,6 @@ class _MyDrawerState extends State<MyDrawer> {
 
 
 
-
-
-      // child: TextField(
-      //
-      //   style: TextStyle(color: color),
-      //   decoration: InputDecoration(
-      //     contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      //     hintText: 'Search',
-      //     hintStyle: TextStyle(color: color),
-      //     prefixIcon: Icon(Icons.search, color: color),
-      //     filled: true,
-      //     fillColor: Colors.white12,
-      //     enabledBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(5),
-      //       borderSide: BorderSide(color: color.withOpacity(0.7)),
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(5),
-      //       borderSide: BorderSide(color: color.withOpacity(0.7)),
-      //     ),
-      //   ),
-      // ),
-   // );
-
-
   Widget buildMenuItem({
     @required String text,
     @required IconData icon,
